432334fcceae95f9e663469662a7955b2bbd6cae
justalinko <alinkokomansuby@gmail.com>
2018-07-10 18:49:22 +0700

    Creating index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* index.php
+*
+* @author Alin Koko Mansuby < alinkokomansuby@gmail.com >
+* 
+* MAFRAME - SIMPLE MVC FRAMEWORK.
+* 
+* @copyright 2018 (c) alinko.
+*
+* Don't publish,recode,modify this script without permission by author.
+**/
+
+require './Maframe/core/MaFrame.php';
+
+MaFrame::run();
